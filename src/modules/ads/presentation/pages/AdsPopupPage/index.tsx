026a51b84a394730922441edd5e902f018c86059
coolbeatz71 <sigmacool@gmail.com>
2025-04-03 00:19:54 +0200

    refactor(ads): use React 19 native title in popup page

--- src/modules/ads/presentation/pages/AdsPopupPage/index.tsx
+++ src/modules/ads/presentation/pages/AdsPopupPage/index.tsx
@@ -1,6 +1,5 @@
 import { Typography } from "antd";
 import type { FC } from "react";
-import { Helmet, HelmetProvider } from "react-helmet-async";
 import { APP_NAME } from "@/shared/infrastructure/constants/common";
 
 const { Title } = Typography;
@@ -13,12 +12,10 @@ const { Title } = Typography;
  */
 const AdsPopupPage: FC = () => {
     return (
-        <HelmetProvider>
-            <Helmet>
-                <title>Popups | {APP_NAME}</title>
-            </Helmet>
+        <>
+            <title>{`Popups | ${APP_NAME}`}</title>
             <Title level={3}>Popups</Title>
-        </HelmetProvider>
+        </>
     );
 };
 
