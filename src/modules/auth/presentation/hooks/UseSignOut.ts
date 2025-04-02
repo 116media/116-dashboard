0a17822c9c36b2a6f051705cc57414d74457dec4
coolbeatz71 <sigmacool@gmail.com>
2025-04-02 13:44:55 +0200

    fix(auth): use backend error messages in sign out hook

--- src/modules/auth/presentation/hooks/UseSignOut.ts
+++ src/modules/auth/presentation/hooks/UseSignOut.ts
@@ -1,7 +1,7 @@
 import { Modal } from "antd";
 import { useNavigate } from "react-router";
 import { signOutAction } from "@/modules/auth/presentation/store/signout.action";
-import { LOGIN_PATH } from "@/shared/infrastructure/constants/paths";
+import { LOGIN_PATH } from "@/shared/presentation/constants/paths";
 import { persistor, useAppDispatch, useAppSelector } from "@/shared/presentation/store/store";
 import { showNotification } from "@/shared/presentation/utils/notification/notification.utils";
 
