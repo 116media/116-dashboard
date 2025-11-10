f5578505ea2a9ae64af0800f434461d9f542c621
coolbeatz71 <sigmacool@gmail.com>
2025-11-10 06:52:17 +0200

    refactor(shared): relocate Icons to shared/ui

---
+++ src/shared/ui/Icons/index.tsx
@@ -0,0 +1,65 @@
+/**
+ * Centralized icon exports from Ant Design Icons.
+ *
+ * @description
+ * Re-exports commonly used Ant Design icons with a consistent naming convention.
+ * All icons are prefixed with "Icon" for easy identification and autocomplete.
+ *
+ * @remarks
+ * - Icons are imported from @ant-design/icons and tree-shaken
+ * - Use these exports instead of importing directly from @ant-design/icons
+ * - Naming pattern: Icon + IconName from Ant Design
+ */
+export {
+    AndroidFilled as IconAndroidFilled,
+    AppleFilled as IconAppleFilled,
+    ArrowLeftOutlined as IconArrowLeftOutlined,
+    BellFilled as IconBellFilled,
+    BellOutlined as IconBellOutlined,
+    BulbFilled as IconBulbFilled,
+    CaretLeftOutlined as IconCaretLeftOutlined,
+    CaretRightFilled as IconCaretRightFilled,
+    CaretRightOutlined as IconCaretRightOutlined,
+    ClockCircleOutlined as IconClockCircleOutlined,
+    CloseCircleFilled as IconCloseCircleFilled,
+    CloseCircleOutlined as IconCloseCircleOutlined,
+    CloseSquareOutlined as IconCloseSquareOutlined,
+    CommentOutlined as IconCommentOutlined,
+    DeleteFilled as IconDeleteFilled,
+    DownOutlined as IconDownOutlined,
+    EditFilled as IconEditFilled,
+    EditOutlined as IconEditOutlined,
+    EllipsisOutlined as IconEllipsisOutlined,
+    ExclamationCircleOutlined as IconExclamationCircleOutlined,
+    FacebookFilled as IconFacebookFilled,
+    FireFilled as IconFireFilled,
+    GoogleOutlined as IconGoogleOutlined,
+    HeartFilled as IconHeartFilled,
+    HeartOutlined as IconHeartOutlined,
+    HomeFilled as IconHomeFilled,
+    HomeOutlined as IconHomeOutlined,
+    InstagramFilled as IconInstagramFilled,
+    LikeFilled as IconLikeFilled,
+    LikeOutlined as IconLikeOutlined,
+    LoadingOutlined as IconLoadingOutlined,
+    LockOutlined as IconLockOutlined,
+    LoginOutlined as IconLoginOutlined,
+    LogoutOutlined as IconLogoutOutlined,
+    MailOutlined as IconMailOutlined,
+    MenuOutlined as IconMenuOutlined,
+    MenuUnfoldOutlined as IconMenuUnfoldOutlined,
+    PlayCircleTwoTone as IconPlayCircleTwoTone,
+    PlusOutlined as IconPlusOutlined,
+    SearchOutlined as IconSearchOutlined,
+    SettingOutlined as IconSettingOutlined,
+    ShareAltOutlined as IconShareAltOutlined,
+    StarFilled as IconStarFilled,
+    StarOutlined as IconStarOutlined,
+    SyncOutlined as IconSyncOutlined,
+    TwitterOutlined as IconTwitterOutlined,
+    UserAddOutlined as IconUserAddOutlined,
+    UserOutlined as IconUserOutlined,
+    VideoCameraFilled as IconVideoCameraFilled,
+    WhatsAppOutlined as IconWhatsAppOutlined,
+    YoutubeFilled as IconYoutubeFilled
+} from "@ant-design/icons";
