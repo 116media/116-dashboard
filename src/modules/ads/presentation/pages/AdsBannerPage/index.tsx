18f4c4c63c3687f9ce0dbc6c9684888e576798ab
coolbeatz71 <sigmacool@gmail.com>
2025-11-11 16:24:03 +0100

    feat(ads): scaffold AdsBannerPage and AdsPopupPage modules

---
+++ src/modules/ads/presentation/pages/AdsBannerPage/index.tsx
@@ -0,0 +1,25 @@
+import { Typography } from "antd";
+import type { FC } from "react";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import { APP_NAME } from "@/shared/lib/constants/common";
+
+const { Title } = Typography;
+
+/**
+ * Banner advertisements management page.
+ *
+ * @component
+ * @returns The ads banner page
+ */
+const AdsBannerPage: FC = () => {
+    return (
+        <HelmetProvider>
+            <Helmet>
+                <title>Bannières | {APP_NAME}</title>
+            </Helmet>
+            <Title level={3}>Bannières</Title>
+        </HelmetProvider>
+    );
+};
+
+export default AdsBannerPage;
