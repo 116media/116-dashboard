71174f5c5a2f0c8774930d32cab2f131d18f78ca
coolbeatz71 <sigmacool@gmail.com>
2025-11-11 16:11:47 +0100

    feat(articles): scaffold ArticlesPage module

---
+++ src/modules/articles/presentation/pages/ArticlesPage/index.tsx
@@ -0,0 +1,25 @@
+import { Typography } from "antd";
+import type { FC } from "react";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import { APP_NAME } from "@/shared/lib/constants/common";
+
+const { Title } = Typography;
+
+/**
+ * Articles management page.
+ *
+ * @component
+ * @returns The articles page
+ */
+const ArticlesPage: FC = () => {
+    return (
+        <HelmetProvider>
+            <Helmet>
+                <title>Articles | {APP_NAME}</title>
+            </Helmet>
+            <Title level={3}>Articles</Title>
+        </HelmetProvider>
+    );
+};
+
+export default ArticlesPage;
