8c4e8c6fcab97d167ab7d074f9e9f229795877c6
coolbeatz71 <sigmacool@gmail.com>
2025-11-11 15:58:31 +0100

    feat(videos): scaffold VideosPage module

---
+++ src/modules/videos/presentation/pages/VideosPage/index.tsx
@@ -0,0 +1,25 @@
+import { Typography } from "antd";
+import type { FC } from "react";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import { APP_NAME } from "@/shared/lib/constants/common";
+
+const { Title } = Typography;
+
+/**
+ * Videos management page.
+ *
+ * @component
+ * @returns The videos page
+ */
+const VideosPage: FC = () => {
+    return (
+        <HelmetProvider>
+            <Helmet>
+                <title>Vidéos | {APP_NAME}</title>
+            </Helmet>
+            <Title level={3}>Vidéos</Title>
+        </HelmetProvider>
+    );
+};
+
+export default VideosPage;
