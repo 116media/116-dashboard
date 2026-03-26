4a88d1c8244684818cec050f38a87a9aaf152fe2
coolbeatz71 <sigmacool@gmail.com>
2026-03-26 04:48:03 +0200

    refactor(auth): update IApiProblemDetails import in login action

--- src/modules/auth/presentation/store/login.action.ts
+++ src/modules/auth/presentation/store/login.action.ts
@@ -5,7 +5,7 @@ import { AuthRepositoryImpl } from "@/modules/auth/infrastructure/repositories/a
 import type { ILoginCredentials } from "@/modules/auth/presentation/model/ILoginCredentials";
 import { authSlice } from "@/modules/auth/presentation/store";
 import { ActionType } from "@/modules/auth/presentation/store/constants";
-import type { IApiProblemDetails } from "@/shared/api/type";
+import type { IApiProblemDetails } from "@/shared/infrastructure/api/type";
 
 const authRepository = new AuthRepositoryImpl();
 const loginUseCase = new LoginUseCase(authRepository);
