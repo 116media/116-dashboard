cdc3dcc0b3e096fbd09bfd7f596206e1c56f7d13
coolbeatz71 <sigmacool@gmail.com>
2026-03-26 05:01:37 +0200

    refactor(auth): update IApiProblemDetails import in resendotp action

--- src/modules/auth/presentation/store/resendotp.action.ts
+++ src/modules/auth/presentation/store/resendotp.action.ts
@@ -5,7 +5,7 @@ import { AuthRepositoryImpl } from "@/modules/auth/infrastructure/repositories/a
 import type { IResendOtpCredentials } from "@/modules/auth/presentation/model/IResendOtpCredentials";
 import { authSlice } from "@/modules/auth/presentation/store";
 import { ActionType } from "@/modules/auth/presentation/store/constants";
-import type { IApiProblemDetails } from "@/shared/api/type";
+import type { IApiProblemDetails } from "@/shared/infrastructure/api/type";
 
 const authRepository = new AuthRepositoryImpl();
 const resendOtpUseCase = new ResendOtpUseCase(authRepository);
