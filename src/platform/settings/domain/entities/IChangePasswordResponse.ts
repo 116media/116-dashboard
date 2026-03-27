eb722fcb592e1d8d915053a98cb4bd4c59f382a7
coolbeatz71 <sigmacool@gmail.com>
2026-03-27 22:40:27 +0200

    feat(settings): add IChangePasswordResponse entity

---
+++ src/platform/settings/domain/entities/IChangePasswordResponse.ts
@@ -0,0 +1,13 @@
+/**
+ * Change password response entity.
+ *
+ * @interface IChangePasswordResponse
+ *
+ * @description
+ * Domain entity representing the response from a change password request.
+ *
+ * @property {boolean} isSuccess - Whether the password change was successful
+ */
+export interface IChangePasswordResponse {
+    isSuccess: boolean;
+}
