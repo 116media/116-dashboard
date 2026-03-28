4cfa6d6704b736678c6fcc437717d6714d0df39c
coolbeatz71 <sigmacool@gmail.com>
2026-03-28 08:06:11 +0200

    feat(settings): add settings form validators

---
+++ src/platform/settings/presentation/utils/validators/settings.validator.ts
@@ -0,0 +1,13 @@
+import type { Rule } from "antd/es/form";
+import { ValidatorUtils } from "@/shared/presentation/utils/validators/validators.utils";
+
+export const SettingsValidator = {
+    userName: (name: string): Rule[] => [
+        ValidatorUtils.required(name),
+        ValidatorUtils.minmax(name, { min: 2, max: 50 })
+    ],
+
+    countryName: (name: string): Rule[] => [ValidatorUtils.required(name)],
+
+    phonePartial: (name: string): Rule[] => [ValidatorUtils.numericOnly(name)]
+} as const;
