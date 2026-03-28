afbdbc34eaadbdd4532f5e4229b18564ebf8eaf2
coolbeatz71 <sigmacool@gmail.com>
2026-03-28 04:45:03 +0200

    feat(settings): add UseProfile hook

---
+++ src/platform/settings/presentation/hooks/UseProfile.ts
@@ -0,0 +1,30 @@
+import type { IUser } from "@/modules/auth/domain/entities/IUser";
+import { authSlice } from "@/modules/auth/presentation/store";
+import { getProfileAction } from "@/platform/settings/presentation/store/profile.action";
+import type { IApiProblemDetails } from "@/shared/infrastructure/api/type";
+import { useAppDispatch, useAppSelector } from "@/shared/presentation/store/store";
+
+interface IUseProfile {
+    profile: IUser;
+    loading: boolean;
+    error: IApiProblemDetails | null | undefined;
+    fetchProfile: () => void;
+}
+
+export const useProfile = (): IUseProfile => {
+    const dispatch = useAppDispatch();
+    const {
+        data: profile,
+        loading,
+        error
+    } = useAppSelector(({ settings: { profile } }) => profile);
+
+    const fetchProfile = async () => {
+        const result = await dispatch(getProfileAction());
+        if (getProfileAction.fulfilled.match(result)) {
+            dispatch(authSlice.actions.updateUser(result.payload));
+        }
+    };
+
+    return { profile, loading, error, fetchProfile };
+};
