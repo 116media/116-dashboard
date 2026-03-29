1aadd637b56fe9748052fd1b9b8b3adc2861c6af
coolbeatz71 <sigmacool@gmail.com>
2026-03-29 12:22:05 +0200

    refactor(settings): use destructured constructor in GetRolesUseCase

--- src/platform/settings/application/usecases/getroles.usecase.ts
+++ src/platform/settings/application/usecases/getroles.usecase.ts
@@ -5,7 +5,11 @@ import type { IUseCase } from "@/shared/application/usecases/IUseCase";
 interface IGetRolesUseCase extends IUseCase<void, IRoleWithPermissions[]> {}
 
 export class GetRolesUseCase implements IGetRolesUseCase {
-    constructor(private readonly settingsRepository: ISettingsRepositoryPort) {}
+    private readonly settingsRepository: ISettingsRepositoryPort;
+
+    constructor({ settingsRepository }: { settingsRepository: ISettingsRepositoryPort }) {
+        this.settingsRepository = settingsRepository;
+    }
 
     async execute(): Promise<IRoleWithPermissions[]> {
         return this.settingsRepository.getRoles();
