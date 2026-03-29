3114878a61656e9a48597863e04deeb515374fb5
coolbeatz71 <sigmacool@gmail.com>
2026-03-29 05:49:54 +0200

    refactor(coming-soon): use centralized IconBellOutlined

--- src/platform/settings/presentation/components/ui/ComingSoon/index.tsx
+++ src/platform/settings/presentation/components/ui/ComingSoon/index.tsx
@@ -1,6 +1,6 @@
-import { BellOutlined } from "@ant-design/icons";
 import { Typography } from "antd";
 import type { FC } from "react";
+import { IconBellOutlined } from "@/shared/presentation/ui/Icons";
 import styles from "./index.module.scss";
 
 const { Title, Paragraph } = Typography;
@@ -8,7 +8,7 @@ const { Title, Paragraph } = Typography;
 const ComingSoon: FC = () => {
     return (
         <div className={styles.container}>
-            <BellOutlined className={styles.icon} />
+            <IconBellOutlined className={styles.icon} />
             <Title level={4}>Bientôt disponible</Title>
             <Paragraph type="secondary">
                 Cette fonctionnalité est en cours de développement. Revenez bientôt !
