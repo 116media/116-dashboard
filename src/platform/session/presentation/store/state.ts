39e16ceddc122ab97ace858335004fbf628b8dd5
coolbeatz71 <sigmacool@gmail.com>
2026-03-30 19:11:52 +0200

    feat(session): add currentUser to session initial state

--- src/platform/session/presentation/store/state.ts
+++ src/platform/session/presentation/store/state.ts
@@ -1,9 +1,11 @@
+import type { IUser } from "@/modules/auth/domain/entities/IUser";
 import type { IRevokeSessionResponse } from "@/platform/session/domain/entities/IRevokeSessionResponse";
 import type { ISession } from "@/platform/session/domain/entities/ISession";
 import { createInitialState } from "@/shared/presentation/store/action.wrapper";
 import type { ISessionState } from "./type";
 
 export const sessionInitialState: ISessionState = {
+    currentUser: createInitialState<IUser>(),
     sessions: createInitialState<ISession[]>(),
     revokeSession: createInitialState<IRevokeSessionResponse>()
 };
