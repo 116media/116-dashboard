c701dca13e06e3afdb048368c261f0d2ab2a417e
coolbeatz71 <sigmacool@gmail.com>
2026-04-02 21:43:52 +0200

    feat(lookup): add update content type credentials

---
+++ src/modules/lookup/presentation/model/IUpdateContentTypeCredentials.ts
@@ -0,0 +1,9 @@
+/**
+ * Form model for updating an existing content type.
+ *
+ * @interface IUpdateContentTypeCredentials
+ * @property {string} name - Content type name (required, max 50 chars)
+ */
+export interface IUpdateContentTypeCredentials {
+    name: string;
+}
