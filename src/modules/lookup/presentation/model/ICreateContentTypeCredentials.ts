ba1c7588cb6eb645e97f295fa577549dc3e50735
coolbeatz71 <sigmacool@gmail.com>
2026-04-02 21:32:01 +0200

    feat(lookup): add create content type credentials

---
+++ src/modules/lookup/presentation/model/ICreateContentTypeCredentials.ts
@@ -0,0 +1,9 @@
+/**
+ * Form model for creating a new content type.
+ *
+ * @interface ICreateContentTypeCredentials
+ * @property {string} name - Content type name (required, max 50 chars)
+ */
+export interface ICreateContentTypeCredentials {
+    name: string;
+}
