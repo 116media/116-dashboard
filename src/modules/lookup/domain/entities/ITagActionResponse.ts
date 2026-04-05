8feaa603bce08f97beed6620f4a39ce3c44ac0aa
coolbeatz71 <sigmacool@gmail.com>
2026-04-05 23:40:22 +0200

    feat(lookup): add ITagActionResponse entity

---
+++ src/modules/lookup/domain/entities/ITagActionResponse.ts
@@ -0,0 +1,9 @@
+/**
+ * Response from tag action endpoints (delete).
+ *
+ * @interface ITagActionResponse
+ * @property {boolean} isSuccess - Whether the operation succeeded
+ */
+export interface ITagActionResponse {
+    isSuccess: boolean;
+}
