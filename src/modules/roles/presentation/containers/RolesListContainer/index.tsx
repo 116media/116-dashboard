0b14053496196acf55fa12695a5c28d58f69d78d
coolbeatz71 <sigmacool@gmail.com>
2026-04-05 10:12:14 +0200

    feat(roles): add resizable columns to roles table

--- src/modules/roles/presentation/containers/RolesListContainer/index.tsx
+++ src/modules/roles/presentation/containers/RolesListContainer/index.tsx
@@ -18,6 +18,7 @@ import CreateEditModal from "@/shared/presentation/ui/CreateEditModal";
 import ErrorAlert from "@/shared/presentation/ui/ErrorAlert";
 import { IconSafetyOutlined } from "@/shared/presentation/ui/Icons";
 import PageHeader from "@/shared/presentation/ui/PageHeader";
+import ResizableTitle from "@/shared/presentation/ui/ResizableTable/ResizableTitle";
 import TableToolbar from "@/shared/presentation/ui/TableToolbar";
 
 /**
@@ -117,6 +118,25 @@ const RolesListContainer: FC = () => {
         }
     };
 
+    const [columnWidths, setColumnWidths] = useState<Record<number, number>>({});
+
+    const handleResize =
+        (index: number) =>
+        (_: React.SyntheticEvent, { size }: { size: { width: number } }) => {
+            setColumnWidths((prev) => ({ ...prev, [index]: size.width }));
+        };
+
+    const baseColumns = rolesTableColumns(handleAction, isSuperAdmin);
+
+    const tableColumns = baseColumns.map((col, index) => ({
+        ...col,
+        width: columnWidths[index] ?? col.width,
+        onHeaderCell: () => ({
+            width: columnWidths[index] ?? col.width,
+            onResize: handleResize(index)
+        })
+    }));
+
     return (
         <>
             <ErrorAlert
@@ -148,8 +168,9 @@ const RolesListContainer: FC = () => {
             <Table
                 rowKey="id"
                 dataSource={rolesList.roles?.items ?? []}
-                columns={rolesTableColumns(handleAction, isSuperAdmin)}
+                columns={tableColumns}
                 loading={rolesList.loading}
+                components={{ header: { cell: ResizableTitle } }}
                 rowSelection={{ type: "checkbox", columnWidth: 48 }}
                 pagination={{
                     showSizeChanger: true,
