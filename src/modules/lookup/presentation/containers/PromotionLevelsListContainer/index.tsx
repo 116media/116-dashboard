7074feb53329fb0730998cb0f4d19baeef83d335
coolbeatz71 <sigmacool@gmail.com>
2026-04-05 15:43:34 +0200

    refactor(lookup): use useResizableColumns hook in promotions

--- src/modules/lookup/presentation/containers/PromotionLevelsListContainer/index.tsx
+++ src/modules/lookup/presentation/containers/PromotionLevelsListContainer/index.tsx
@@ -11,11 +11,12 @@ import { useCreatePromotionLevel } from "@/modules/lookup/presentation/hooks/Use
 import { usePromotionLevelActions } from "@/modules/lookup/presentation/hooks/UsePromotionLevelActions";
 import { usePromotionLevelsList } from "@/modules/lookup/presentation/hooks/UsePromotionLevelsList";
 import { useUpdatePromotionLevel } from "@/modules/lookup/presentation/hooks/UseUpdatePromotionLevel";
+import { useResizableColumns } from "@/shared/presentation/hooks/UseResizableColumns";
 import CreateEditModal from "@/shared/presentation/ui/CreateEditModal";
 import ErrorAlert from "@/shared/presentation/ui/ErrorAlert";
 import { IconStarOutlined } from "@/shared/presentation/ui/Icons";
 import PageHeader from "@/shared/presentation/ui/PageHeader";
-import ResizableTitle from "@/shared/presentation/ui/ResizableTable/ResizableTitle";
+import ResizableTitle from "@/shared/presentation/ui/ResizableTable";
 import TableToolbar from "@/shared/presentation/ui/TableToolbar";
 
 /**
@@ -74,24 +75,9 @@ const PromotionLevelsListContainer: FC = () => {
         }
     };
 
-    const [columnWidths, setColumnWidths] = useState<Record<number, number>>({});
-
-    const handleResize =
-        (index: number) =>
-        (_: React.SyntheticEvent, { size }: { size: { width: number } }) => {
-            setColumnWidths((prev) => ({ ...prev, [index]: size.width }));
-        };
-
-    const baseColumns = promotionLevelsTableColumns(handleAction, isSuperAdmin);
-
-    const tableColumns = baseColumns.map((col, index) => ({
-        ...col,
-        width: columnWidths[index] ?? col.width,
-        onHeaderCell: () => ({
-            width: columnWidths[index] ?? col.width,
-            onResize: handleResize(index)
-        })
-    }));
+    const { columns: tableColumns } = useResizableColumns(
+        promotionLevelsTableColumns(handleAction, isSuperAdmin)
+    );
 
     return (
         <>
