02325931b1bba5bc903a294e809676009e333a41
coolbeatz71 <sigmacool@gmail.com>
2026-04-08 01:01:54 +0200

    feat(catalog): add ICreatePackageCredentials form model

---
+++ src/modules/catalog/presentation/model/ICreatePackageCredentials.ts
@@ -0,0 +1,13 @@
+/**
+ * Form model for creating a new package.
+ *
+ * @interface ICreatePackageCredentials
+ * @property {string} name - Package name (required, max 100 chars)
+ * @property {string} [description] - Package description (optional, max 500 chars)
+ * @property {number} flatPriceUsd - Flat price in USD (required)
+ */
+export interface ICreatePackageCredentials {
+    name: string;
+    description?: string;
+    flatPriceUsd: number;
+}
