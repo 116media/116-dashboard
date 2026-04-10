1549450599e262af048eece7afa9a5a3e11673e4
coolbeatz71 <sigmacool@gmail.com>
2026-04-10 08:50:17 +0200

    feat(catalog): add CategoryPricingList skeleton loading

---
+++ src/modules/catalog/presentation/components/ui/CategoryPricingList/CategoryPricingList.Loading.tsx
@@ -0,0 +1,33 @@
+import { Card, Flex, Skeleton, Space } from "antd";
+import type { FC } from "react";
+import styles from "./index.module.scss";
+
+/**
+ * Skeleton loading state for the CategoryPricingList component.
+ *
+ * @component
+ *
+ * @description
+ * Renders placeholder cards matching the shape of pricing tier
+ * cards (tier name + price on the left, action buttons on the right).
+ */
+const CategoryPricingListLoading: FC = () => (
+    <Space direction="vertical" style={{ width: "100%" }} size="small" className={styles.categoryPricingList}>
+        {Array.from({ length: 3 }, (_, i) => i).map((key) => (
+            <Card key={key} size="small" hoverable>
+                <Flex justify="space-between" align="center">
+                    <Flex gap={12} align="center">
+                        <Skeleton.Input active size="small" style={{ width: 100 }} />
+                        <Skeleton.Input active size="small" style={{ width: 60 }} />
+                    </Flex>
+                    <Flex gap={6}>
+                        <Skeleton.Button active size="small" shape="square" />
+                        <Skeleton.Button active size="small" shape="square" />
+                    </Flex>
+                </Flex>
+            </Card>
+        ))}
+    </Space>
+);
+
+export default CategoryPricingListLoading;
