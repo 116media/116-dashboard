835b40e028c23686ba8291f4cf3a6c1981df2183
coolbeatz71 <sigmacool@gmail.com>
2026-04-15 07:44:41 +0200

    refactor(roles): use credential interfaces in execute

--- src/modules/roles/application/usecases/bulkupdatepermissions.usecase.ts
+++ src/modules/roles/application/usecases/bulkupdatepermissions.usecase.ts
@@ -33,13 +33,10 @@ export class BulkUpdatePermissionsUseCase implements IBulkUpdatePermissionsUseCa
     /**
      * Executes the bulk update permissions use case.
      *
-     * @param {{ roleId: string; permissionIds: string[] }} params - Role UUID and desired permission UUIDs
+     * @param {{ roleId: string; permissionIds: string[] }} request - Role UUID and desired permission UUIDs
      * @returns {Promise<Result<IRoleWithPermissions>>} `ok(IRoleWithPermissions)` on success, `err(Failure)` on failure
      */
-    async execute(params: {
-        roleId: string;
-        permissionIds: string[];
-    }): Promise<Result<IRoleWithPermissions>> {
-        return this.rolesRepository.bulkUpdatePermissions(params.roleId, params.permissionIds);
+    async execute(request: { roleId: string; permissionIds: string[] }): Promise<Result<IRoleWithPermissions>> {
+        return this.rolesRepository.bulkUpdatePermissions(request.roleId, request.permissionIds);
     }
 }
