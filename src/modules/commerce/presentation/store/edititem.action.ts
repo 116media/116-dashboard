2b473bd50ca5f48662a4e96677c318e703e9de2a
coolbeatz71 <sigmacool@gmail.com>
2026-04-16 11:34:49 +0200

    refactor(commerce): use action response entity in thunk

--- src/modules/commerce/presentation/store/edititem.action.ts
+++ src/modules/commerce/presentation/store/edititem.action.ts
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import type { IOrderItemEntity } from "@/modules/commerce/domain/entities/IOrderItemEntity";
+import type { IEditItemCredentials } from "@/modules/commerce/presentation/model/IEditItemCredentials";
 import type { Failure } from "@/shared/domain/failures/failure";
-import type { EnumCoreContentType } from "@/shared/infrastructure/api/generated/116.api";
 import container from "@/shared/infrastructure/service.locator";
 import { ActionType } from "./constants";
 
@@ -15,15 +15,7 @@ import { ActionType } from "./constants";
  */
 export const editItemAction = createAsyncThunk<
     IOrderItemEntity,
-    {
-        orderId: string;
-        itemId: string;
-        contentKind?: EnumCoreContentType;
-        categoryId?: string;
-        promotionLevelId?: string | null;
-        socialBoost?: boolean;
-        isBonus?: boolean;
-    },
+    { orderId: string; itemId: string; data: IEditItemCredentials },
     { rejectValue: Failure }
 >(ActionType.EditItem, async (params, { rejectWithValue }) => {
     const result = await container.cradle.editItemUseCase.execute(params);
