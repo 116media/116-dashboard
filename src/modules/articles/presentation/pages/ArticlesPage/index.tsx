ee9937b9fc9f30b278f6f8e6cb5a1ed41582103f
coolbeatz71 <sigmacool@gmail.com>
2026-04-19 07:29:28 +0200

    refactor(articles): add page wrapper with background styles

--- src/modules/articles/presentation/pages/ArticlesPage/index.tsx
+++ src/modules/articles/presentation/pages/ArticlesPage/index.tsx
@@ -1,6 +1,7 @@
 import type { FC } from "react";
 import ArticlesListContainer from "@/modules/articles/presentation/containers/ArticlesListContainer";
 import { APP_NAME } from "@/shared/infrastructure/constants/common";
+import styles from "./index.module.scss";
 
 /**
  * Articles management page.
@@ -13,10 +14,10 @@ import { APP_NAME } from "@/shared/infrastructure/constants/common";
  * @returns The articles page
  */
 const ArticlesPage: FC = () => (
-    <>
+    <div className={styles.page}>
         <title>{`Articles | ${APP_NAME}`}</title>
         <ArticlesListContainer />
-    </>
+    </div>
 );
 
 export default ArticlesPage;
