9acd1c1e57306a4ca1e0b1fae39f9209f2b60455
coolbeatz71 <sigmacool@gmail.com>
2026-04-26 12:10:39 +0200

    feat(articles-store): add resetRejectArticleAction

--- src/modules/articles/presentation/store/rejectarticle.action.ts
+++ src/modules/articles/presentation/store/rejectarticle.action.ts
@@ -1,8 +1,12 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import type { IArticleActionResponse } from "@/modules/articles/domain/entities/IArticleActionResponse";
+import { articlesSlice } from "@/modules/articles/presentation/store";
 import type { Failure } from "@/shared/domain/failures/failure";
 import container from "@/shared/infrastructure/service.locator";
 import { ActionType } from "./constants";
+
+export const resetRejectArticleAction = () =>
+    articlesSlice.actions.clear({ context: ActionType.RejectArticle });
 
 /**
  * Async thunk to reject an article with a reason.
