b5edd1d5c8cb32fcc8decfac70fc5b6a55aba56e
coolbeatz71 <sigmacool@gmail.com>
2026-04-27 05:29:27 +0200

    refactor(package-credentials): remove flatPriceUsd field

--- src/modules/catalog/presentation/model/ICreatePackageCredentials.ts
+++ src/modules/catalog/presentation/model/ICreatePackageCredentials.ts
@@ -4,10 +4,8 @@
  * @interface ICreatePackageCredentials
  * @property {string} name - Package name (required, max 100 chars)
  * @property {string} description - Package description (required, max 500 chars)
- * @property {number} flatPriceUsd - Flat price in USD (required)
  */
 export interface ICreatePackageCredentials {
     name: string;
     description: string;
-    flatPriceUsd: number;
 }
