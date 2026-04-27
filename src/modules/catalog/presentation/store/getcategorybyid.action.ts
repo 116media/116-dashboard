5e20233198403d95b8c9efc55bfc3b6406eeb885
coolbeatz71 <sigmacool@gmail.com>
2026-04-27 08:12:55 +0200

    feat(catalog-store): add resetGetCategoryByIdAction

--- src/modules/catalog/presentation/store/getcategorybyid.action.ts
+++ src/modules/catalog/presentation/store/getcategorybyid.action.ts
@@ -1,8 +1,12 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import type { ICategoryEntity } from "@/modules/catalog/domain/entities/ICategoryEntity";
+import { catalogSlice } from "@/modules/catalog/presentation/store";
 import type { Failure } from "@/shared/domain/failures/failure";
 import container from "@/shared/infrastructure/service.locator";
 import { ActionType } from "./constants";
+
+export const resetGetCategoryByIdAction = () =>
+    catalogSlice.actions.clear({ context: ActionType.GetCategoryById });
 
 /**
  * Async thunk to fetch a single category by ID.
