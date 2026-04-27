5058ae501a42d838a0e51b90cbd4157a256bd449
coolbeatz71 <sigmacool@gmail.com>
2026-04-27 06:19:45 +0200

    feat(catalog-store): add resetAddPackageSlotAction

--- src/modules/catalog/presentation/store/addpackageslot.action.ts
+++ src/modules/catalog/presentation/store/addpackageslot.action.ts
@@ -1,8 +1,12 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import type { IPackageSlotEntity } from "@/modules/catalog/domain/entities/IPackageSlotEntity";
+import { catalogSlice } from "@/modules/catalog/presentation/store";
 import type { Failure } from "@/shared/domain/failures/failure";
 import container from "@/shared/infrastructure/service.locator";
 import { ActionType } from "./constants";
+
+export const resetAddPackageSlotAction = () =>
+    catalogSlice.actions.clear({ context: ActionType.AddPackageSlot });
 
 /**
  * Async thunk to add a slot to a package.
