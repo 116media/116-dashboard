bde8bd29e5e0130f93d0fc7f37e854f2827530bc
coolbeatz71 <sigmacool@gmail.com>
2026-04-27 15:33:03 +0200

    feat(commerce-store): add resetListPaymentsAction

--- src/modules/commerce/presentation/store/listpayments.action.ts
+++ src/modules/commerce/presentation/store/listpayments.action.ts
@@ -1,10 +1,14 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import type { IPaymentSummaryEntity } from "@/modules/commerce/domain/entities/IPaymentSummaryEntity";
 import type { IPaymentsQueryParams } from "@/modules/commerce/presentation/model/IPaymentsQueryParams";
+import { commerceSlice } from "@/modules/commerce/presentation/store";
 import type { Failure } from "@/shared/domain/failures/failure";
 import type { IPaginatedResult } from "@/shared/domain/types/pagination";
 import container from "@/shared/infrastructure/service.locator";
 import { ActionType } from "./constants";
+
+export const resetListPaymentsAction = () =>
+    commerceSlice.actions.clear({ context: ActionType.ListPayments });
 
 /**
  * Async thunk to fetch a paginated list of payments.
