28604d9abfe25e6c957de7e75be39e58caf2a53c
coolbeatz71 <sigmacool@gmail.com>
2026-04-28 10:55:50 +0200

    feat(lookup-store): add resetGetContentTypesAction

--- src/modules/lookup/presentation/store/getcontenttypes.action.ts
+++ src/modules/lookup/presentation/store/getcontenttypes.action.ts
@@ -1,8 +1,12 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import type { IContentTypeEntity } from "@/modules/lookup/domain/entities/IContentTypeEntity";
+import { lookupSlice } from "@/modules/lookup/presentation/store";
 import type { Failure } from "@/shared/domain/failures/failure";
 import container from "@/shared/infrastructure/service.locator";
 import { ActionType } from "./constants";
+
+export const resetGetContentTypesAction = () =>
+    lookupSlice.actions.clear({ context: ActionType.GetContentTypes });
 
 /**
  * Async thunk to fetch all content types.
