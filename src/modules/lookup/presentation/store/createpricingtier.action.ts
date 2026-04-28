f1d38f8df45e7ba749ed12811dd7e930541800af
coolbeatz71 <sigmacool@gmail.com>
2026-04-28 09:27:49 +0200

    feat(lookup-store): add resetCreatePricingTierAction

--- src/modules/lookup/presentation/store/createpricingtier.action.ts
+++ src/modules/lookup/presentation/store/createpricingtier.action.ts
@@ -1,8 +1,12 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import type { IPricingTierEntity } from "@/modules/lookup/domain/entities/IPricingTierEntity";
+import { lookupSlice } from "@/modules/lookup/presentation/store";
 import type { Failure } from "@/shared/domain/failures/failure";
 import container from "@/shared/infrastructure/service.locator";
 import { ActionType } from "./constants";
+
+export const resetCreatePricingTierAction = () =>
+    lookupSlice.actions.clear({ context: ActionType.CreatePricingTier });
 
 /**
  * Async thunk to create a new pricing tier.
