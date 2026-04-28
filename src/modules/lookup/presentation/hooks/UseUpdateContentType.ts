3018652c6c7eb51e791513c0014fb94e7f551f39
coolbeatz71 <sigmacool@gmail.com>
2026-04-28 07:47:13 +0200

    feat(use-update-content-type): dispatch reset action and clear form in resetUpdate

--- src/modules/lookup/presentation/hooks/UseUpdateContentType.ts
+++ src/modules/lookup/presentation/hooks/UseUpdateContentType.ts
@@ -3,7 +3,10 @@ import { Form } from "antd";
 import { useEffect, useState } from "react";
 import type { IContentTypeEntity } from "@/modules/lookup/domain/entities/IContentTypeEntity";
 import type { IUpdateContentTypeCredentials } from "@/modules/lookup/presentation/model/IUpdateContentTypeCredentials";
-import { updateContentTypeAction } from "@/modules/lookup/presentation/store/updatecontenttype.action";
+import {
+    resetUpdateContentTypeAction,
+    updateContentTypeAction
+} from "@/modules/lookup/presentation/store/updatecontenttype.action";
 import { ContentTypesNotification } from "@/modules/lookup/presentation/utils/notification/lookup.content-types.notification";
 import type { Failure } from "@/shared/domain/failures/failure";
 import { useAppDispatch, useAppSelector } from "@/shared/presentation/store/store";
@@ -71,6 +74,8 @@ export const useUpdateContentType = (
 
     const resetUpdate = () => {
         setSuccess(null);
+        form.resetFields();
+        dispatch(resetUpdateContentTypeAction());
         form.resetFields();
     };
 
