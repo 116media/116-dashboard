a6c64582b07657181d793d42e513c7fa23b9f204
coolbeatz71 <sigmacool@gmail.com>
2026-04-28 15:32:29 +0200

    feat(permissions-store): add resetCreatePermissionAction

--- src/modules/permissions/presentation/store/create.action.ts
+++ src/modules/permissions/presentation/store/create.action.ts
@@ -1,9 +1,13 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import type { IPermissionEntity } from "@/modules/permissions/domain/entities/IPermission";
 import type { ICreatePermissionCredentials } from "@/modules/permissions/presentation/model/ICreatePermissionCredentials";
+import { permissionsSlice } from "@/modules/permissions/presentation/store";
 import type { Failure } from "@/shared/domain/failures/failure";
 import container from "@/shared/infrastructure/service.locator";
 import { ActionType } from "./constants";
+
+export const resetCreatePermissionAction = () =>
+    permissionsSlice.actions.clear({ context: ActionType.CreatePermission });
 
 /**
  * Async thunk to create a new permission.
