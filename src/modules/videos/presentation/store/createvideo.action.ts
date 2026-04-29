4004c310aa21ad18c20a110e487e3d9533fccfa3
coolbeatz71 <sigmacool@gmail.com>
2026-04-29 13:39:45 +0200

    feat(videos-store): add resetCreateVideoAction

--- src/modules/videos/presentation/store/createvideo.action.ts
+++ src/modules/videos/presentation/store/createvideo.action.ts
@@ -1,8 +1,12 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import type { IVideoEntity } from "@/modules/videos/domain/entities/IVideoEntity";
+import { videosSlice } from "@/modules/videos/presentation/store";
 import type { Failure } from "@/shared/domain/failures/failure";
 import container from "@/shared/infrastructure/service.locator";
 import { ActionType } from "./constants";
+
+export const resetCreateVideoAction = () =>
+    videosSlice.actions.clear({ context: ActionType.CreateVideo });
 
 /**
  * Async thunk to create a new video.
