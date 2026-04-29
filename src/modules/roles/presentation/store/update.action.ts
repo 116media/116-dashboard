54748551a751b86fc7a6ce441edec330f674e4a5
coolbeatz71 <sigmacool@gmail.com>
2026-04-29 08:50:32 +0200

    feat(roles-store): add resetUpdateRoleAction

--- src/modules/roles/presentation/store/update.action.ts
+++ src/modules/roles/presentation/store/update.action.ts
@@ -1,9 +1,13 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import type { IRoleEntity } from "@/modules/roles/domain/entities/IRole";
 import type { IUpdateRoleCredentials } from "@/modules/roles/presentation/model/IUpdateRoleCredentials";
+import { rolesSlice } from "@/modules/roles/presentation/store";
 import type { Failure } from "@/shared/domain/failures/failure";
 import container from "@/shared/infrastructure/service.locator";
 import { ActionType } from "./constants";
+
+export const resetUpdateRoleAction = () =>
+    rolesSlice.actions.clear({ context: ActionType.UpdateRole });
 
 /**
  * Async thunk to update an existing role's name and/or description.
