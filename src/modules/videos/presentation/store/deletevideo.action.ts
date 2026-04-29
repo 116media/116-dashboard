6194fc88d56c0ccaa1836bd01c458e7054323806
coolbeatz71 <sigmacool@gmail.com>
2026-04-29 13:52:20 +0200

    feat(videos-store): add resetDeleteVideoAction

--- src/modules/videos/presentation/store/deletevideo.action.ts
+++ src/modules/videos/presentation/store/deletevideo.action.ts
@@ -1,8 +1,12 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import type { IVideoActionResponse } from "@/modules/videos/domain/entities/IVideoActionResponse";
+import { videosSlice } from "@/modules/videos/presentation/store";
 import type { Failure } from "@/shared/domain/failures/failure";
 import container from "@/shared/infrastructure/service.locator";
 import { ActionType } from "./constants";
+
+export const resetDeleteVideoAction = () =>
+    videosSlice.actions.clear({ context: ActionType.DeleteVideo });
 
 /**
  * Async thunk to delete a video.
