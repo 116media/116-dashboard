b28c7a4beacbac4149b809cd8ebf808463dd4fd3
coolbeatz71 <sigmacool@gmail.com>
2026-04-29 06:57:21 +0200

    feat(roles-store): add resetBulkUpdatePermissionsAction

--- src/modules/roles/presentation/store/bulkupdatepermissions.action.ts
+++ src/modules/roles/presentation/store/bulkupdatepermissions.action.ts
@@ -1,8 +1,12 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import type { IRoleWithPermissions } from "@/modules/roles/domain/entities/IRoleWithPermissions";
+import { rolesSlice } from "@/modules/roles/presentation/store";
 import type { Failure } from "@/shared/domain/failures/failure";
 import container from "@/shared/infrastructure/service.locator";
 import { ActionType } from "./constants";
+
+export const resetBulkUpdatePermissionsAction = () =>
+    rolesSlice.actions.clear({ context: ActionType.BulkUpdatePermissions });
 
 /**
  * Async thunk to replace all permissions on a role.
