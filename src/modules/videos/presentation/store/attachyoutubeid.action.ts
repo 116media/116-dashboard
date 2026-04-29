9e74987d118d1c25ffc3b7859c035ccb3831ff73
coolbeatz71 <sigmacool@gmail.com>
2026-04-29 13:27:11 +0200

    feat(videos-store): add resetAttachYoutubeIdAction

--- src/modules/videos/presentation/store/attachyoutubeid.action.ts
+++ src/modules/videos/presentation/store/attachyoutubeid.action.ts
@@ -1,8 +1,12 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import type { IVideoEntity } from "@/modules/videos/domain/entities/IVideoEntity";
+import { videosSlice } from "@/modules/videos/presentation/store";
 import type { Failure } from "@/shared/domain/failures/failure";
 import container from "@/shared/infrastructure/service.locator";
 import { ActionType } from "./constants";
+
+export const resetAttachYoutubeIdAction = () =>
+    videosSlice.actions.clear({ context: ActionType.AttachYoutubeId });
 
 /**
  * Async thunk to attach a YouTube video ID to a video.
