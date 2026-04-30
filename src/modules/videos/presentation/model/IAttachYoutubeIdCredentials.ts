07eff39319f200c0a806e23bff1c79fa42057762
coolbeatz71 <sigmacool@gmail.com>
2026-04-30 22:57:33 +0200

    refactor(youtube): remove YoutubeId-named files, superseded by YoutubeUrl:   - delete YoutubeIdForm component   - delete YoutubeIdModal component   - delete UseAttachYoutubeId hook   - delete IAttachYoutubeIdCredentials model

--- src/modules/videos/presentation/model/IAttachYoutubeIdCredentials.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Form model for attaching a YouTube video ID to a video.
- *
- * @interface IAttachYoutubeIdCredentials
- * @property {string} youtubeVideoId - YouTube video identifier (required, max 20 chars)
- */
-export interface IAttachYoutubeIdCredentials {
-    youtubeVideoId: string;
-}
