13034c45b3eb93cdce4c47791e4036d8c33afebf
coolbeatz71 <sigmacool@gmail.com>
2026-05-02 15:34:02 +0200

    refactor(videos): accept dayjs object for featured date field

--- src/modules/videos/presentation/model/IUpdateVideoCredentials.ts
+++ src/modules/videos/presentation/model/IUpdateVideoCredentials.ts
@@ -1,3 +1,5 @@
+import type { Dayjs } from "dayjs";
+
 /**
  * Form model for updating an existing video.
  *
@@ -10,7 +12,7 @@
  * @property {string | null} [orderItemId] - Optional order item linked to the video
  * @property {boolean} socialBoost - Whether social boost is enabled
  * @property {boolean} isFeatured - Whether the video is featured
- * @property {string | null} [featuredUntil] - Optional ISO date until which the video is featured
+ * @property {Dayjs | string | null} [featuredUntil] - Optional date until which the video is featured
  * @property {string | null} [metaTitle] - Optional SEO meta title
  * @property {string | null} [metaDescription] - Optional SEO meta description
  */
@@ -23,7 +25,7 @@ export interface IUpdateVideoCredentials {
     orderItemId?: string | null;
     socialBoost: boolean;
     isFeatured: boolean;
-    featuredUntil?: string | null;
+    featuredUntil?: Dayjs | string | null;
     metaTitle?: string | null;
     metaDescription?: string | null;
 }
