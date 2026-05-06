26452da57bf525cbfd2ef20d2957707ff0a52cc5
coolbeatz71 <sigmacool@gmail.com>
2026-05-06 10:14:12 +0200

    refactor(lyrics-seo): update IUpdateLyricsSeoCredentials model

--- src/modules/lyrics/presentation/model/IUpdateLyricsSeoCredentials.ts
+++ src/modules/lyrics/presentation/model/IUpdateLyricsSeoCredentials.ts
@@ -4,10 +4,8 @@
  * @interface IUpdateLyricsSeoCredentials
  * @property {string} metaTitle - SEO meta title (required, max 70 chars)
  * @property {string} metaDescription - SEO meta description (required, max 160 chars)
- * @property {string} [metaKeywords] - SEO meta keywords (optional, max 250 chars)
  */
 export interface IUpdateLyricsSeoCredentials {
     metaTitle: string;
     metaDescription: string;
-    metaKeywords?: string;
 }
