85b8d060c95bdd3aaeda53eac68d5cc8b2d7308d
coolbeatz71 <sigmacool@gmail.com>
2026-05-15 09:32:51 +0200

    feat(articles-list): wire onUnpromoteSubmit to workflow hook

--- src/modules/articles/presentation/containers/ArticlesListContainer/index.tsx
+++ src/modules/articles/presentation/containers/ArticlesListContainer/index.tsx
@@ -175,6 +175,14 @@ const ArticlesListContainer: FC = () => {
                     });
                     if (success) modals.setActionOpen(false);
                 }}
+                onUnpromoteSubmit={async (values) => {
+                    if (!modals.selectedEntity) return;
+                    const success = await workflow.onUnpromote({
+                        slug: modals.selectedEntity.slug,
+                        data: values
+                    });
+                    if (success) modals.setActionOpen(false);
+                }}
                 onCancel={() => modals.setActionOpen(false)}
             />
 
