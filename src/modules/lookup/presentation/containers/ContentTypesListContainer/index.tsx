614728261acbec48ee380d1b3336f92bf8cc420b
coolbeatz71 <sigmacool@gmail.com>
2026-06-22 11:20:21 +0200

    feat(lookup): reset content type error after modal closes

--- src/modules/lookup/presentation/containers/ContentTypesListContainer/index.tsx
+++ src/modules/lookup/presentation/containers/ContentTypesListContainer/index.tsx
@@ -150,6 +150,7 @@ const ContentTypesListContainer: FC = () => {
                     })
                 }
                 onCancel={() => modals.setActionOpen(false)}
+                onAfterClose={actions.resetActionError}
             />
         </>
     );
